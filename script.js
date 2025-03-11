ca2e474ef2ae6fd57115daf698155cd5bf906cdb
shivrajsingh123 <45819719+shivrajsingh123@users.noreply.github.com>
2025-03-11 11:55:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function stringChop(str, size) {
-  // your code here
-}
+  // your code her
+ if (!str) return []; // Return an empty array if the input string is null or empty
+
+    const chunks = [];
+    for (let i = 0; i < str.length; i += size) {
+        chunks.push(str.substring(i, i + size));
+    }
+    
+    return chunks;
 
 // Do not change the code below
 const str = prompt("Enter String.");
